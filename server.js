7a5ab11b3af2089d3bee2fba880043f943f51996
mikcro <nowitsforever@gmail.com>
2017-10-30 18:21:33 -0500

    trying process cwd

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(process.cwd() + "/public")));
 app.set('json spaces', 4);
 
 
