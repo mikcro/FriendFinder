af7097ef9497c299963cedfe6c67a6918b700035
mikcro <nowitsforever@gmail.com>
2017-10-31 09:09:49 -0500

    path

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-app.use(express.static(path.join(process.cwd() + "/public")));
+app.use(express.static(path.join(__dirname, "app/public")));
+
 app.set('json spaces', 4);
 
 
